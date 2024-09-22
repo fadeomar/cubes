b5edaf0c50e8e8c9baadce42aa9a125a7ec22ae0
fadeomar <fadeomar2015@gmail.com>
2024-09-22 18:31:47 +0300

    create Drink Water App

---
+++ Apps/Drink_Water/app.js
@@ -0,0 +1,56 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.querySelector("#liters");
+const percentage = document.querySelector("#percentage");
+const remained = document.querySelector("#remained");
+
+updateBigCup();
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => {
+    highlightCups(idx);
+  });
+});
+
+function highlightCups(index) {
+  if (index === 7 && smallCups[index].classList.contains("full")) {
+    index--;
+  } else if (
+    smallCups[index].classList.contains("full") &&
+    !smallCups[index].nextElementSibling.classList.contains("full")
+  ) {
+    index--;
+  }
+
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= index) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+
+    percentage.textContent = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.textContent = `${2 - (250 * fullCups) / 1000}L`;
+  }
+}
